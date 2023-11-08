36510d62617ecf93a509f0bb39eb2caefd4a3578
ozcanbayram <ozzcanbayram@gmail.com>
2023-11-08 18:46:21 +0300

    First screens UI complated.

---
+++ app/src/main/java/com/ozcanbayram/catchthejerry/FirstScreen.kt
@@ -0,0 +1,11 @@
+package com.ozcanbayram.catchthejerry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_screen)
+    }
+}
